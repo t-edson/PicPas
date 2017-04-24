70912677aea2e7b2543346fd930aef2c539a2309
Tito Hinostroza <dev_null_1@hotmail.com>
2017-04-23 20:57:06 -0500

    Add files via upload

--- SinNombre.pas
+++ SinNombre.pas
@@ -1,28 +1,21 @@
-{Sample program to blink a Led on PORTB.4}
 {$FREQUENCY 8 MHZ }
 {$PROCESSOR PIC16F84A}
 program BlinkLed;
 var
-  PORTB : BYTE absolute $06;
-  TRISB : BYTE absolute $86;
-//  pin: bit absolute PORTB.7;
+  GPIO : BYTE absolute $06;
+  TRISIO : BYTE absolute $86;
   //Define output
-  out1: boolean absolute PORTB.0;
-  //Define inputs 
-  in1: boolean absolute PORTB.1;
-  in2: boolean absolute PORTB.2;
-  in3: boolean absolute PORTB.3;
-begin                   
-  //Set port direction       
-  TRISB := %00000110; 
-  
+  out1: boolean absolute GPIO.0;
+  //Define inputs
+  in1: boolean absolute GPIO.1;
+  in2: boolean absolute GPIO.2;
+begin
+  //Set port direction
+  TRISIO := %00000110;
   while true do begin
-    in1 and in2;
-    if in1 and in2 then 
-      out1 := true
-    else 
- 	    out1 := false;
-    delay_ms(500);
+	  out1 := in1 and not in2; 
 //    out1 := not out1;
+    delay_ms(200);
   end;
 end.
+
