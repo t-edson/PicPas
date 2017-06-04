8e302f14d6797ff31e33466f18010a2d0e1647dd
Tito Hinostroza <dev_null_1@hotmail.com>
2017-06-04 17:09:25 -0500

    Delete tra_FrameSyntaxTree.pas

--- tra_FrameSyntaxTree.pas
+++
@@ -1,8 +0,0 @@
-TIT_MAIN := trans('Program','Programa','','');
-TIT_UNIT := trans('Units','Unidades','','');
-TIT_CONS := trans('Constants','Constantes','','');
-TIT_VARS := trans('Variables','Variables','','');
-TIT_FUNC := trans('Functions','Funciones','','');
-TIT_OTHER:= trans('Others','Otros','','');
-Label1.Caption := Trans('Code Explorer', 'Explorador de Código','','');
-
