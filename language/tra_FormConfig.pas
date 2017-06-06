6bb6bf7000397ba802ef9f1622ca2344dfa49660
Tito Hinostroza <dev_null_1@hotmail.com>
2017-06-06 17:08:28 -0500

    Add files via upload

--- language/tra_FormConfig.pas
+++ language/tra_FormConfig.pas
@@ -1,25 +1,29 @@
-Caption              := Trans('Settings'             , 'Configuración', '','Einstellungen');
+Caption              := Trans('Settings'               , 'Configuración'            , '', 'Einstellungen');
 //General settings
-tabGeneral.Caption   := Trans('General'              , 'General', '','Allgemein');
-RadioGroup1.Caption  := Trans('Toolbar'              , 'Barra de herramientas', '','Werkzeugleiste');
-RadioGroup1.Items[0] := Trans('Small Icons'          , 'Íconos pequeños', '','Kleine Bilder');
-RadioGroup1.Items[1] := Trans('Big Icons'            , 'Íconos grandes', '','Große Bilder');
-label1.Caption       := Trans('Units Path:'          , 'Ruta de unidades', '','Unitpfad:');
-chkLoadLast.Caption  := Trans('Load last file edited', 'Cargar último archivo editado', '','Letz editierte Datei laden');
-Label2.Caption       := Trans('Language'             , 'Lenguaje', '','Sprache');
+tabGeneral.Caption   := Trans('General'                , 'General'                  , '', 'Allgemein');
+RadioGroup1.Caption  := Trans('Toolbar'                , 'Barra de herramientas'    , '', 'Werkzeugleiste');
+RadioGroup1.Items[0] := Trans('Small Icons'            , 'Íconos pequeños'          , '', 'Kleine Bilder');
+RadioGroup1.Items[1] := Trans('Big Icons'              , 'Íconos grandes'           , '', 'Große Bilder');
+label1.Caption       := Trans('Units Path:'            , 'Ruta de unidades'         , '', 'Unitpfad:');
+chkLoadLast.Caption  := Trans('Load last file edited'  , 'Cargar último archivo editado','','Letz editierte Datei laden');
+Label2.Caption       := Trans('Language'               , 'Lenguaje'                 , '', 'Sprache');
 //Editor Settings
-tabEditor.Caption    := Trans('Editor'               , 'Editor', '','Editor');
+tabEditor.Caption    := Trans('Editor'                 , 'Editor'                   , '', 'Editor');
 //Assembler settings
-tabEnsamb.Caption    := Trans('Assembler'            , 'Ensamblador', '','Assembler');
-chkIncHeadMpu.Caption:= Trans('Include MPU &Header'  , 'Incluir &Encabezado de MPU', '','MPU &Kopfzeilen einfügen');
-chkIncDecVar.Caption := Trans('Include &Variables declaration', 'Incluir Declaración de variables', '','Variablendeklaration einfügen');
-RadioGroup2.Caption  := Trans('Style'                , 'Estilo', '','Stil');
-chkExcUnus.Caption   := Trans('Exclude unused'       , 'Excluir no usadas', '','Unbenutzte ausschließen');
-chkIncAddress.Caption:= Trans('Include &Memory Address','Incluir &Dirección de memoria', '','Speicheradressen zufügen');
-chkIncComment.Caption:= Trans('Include &Comments'    , 'Incluir &Comentarios', '','Kommentare hinzufügen');
+tabEnsamb.Caption    := Trans('Assembler'              , 'Ensamblador'              , '', 'Assembler');
+chkIncHeadMpu.Caption:= Trans('Include MPU &Header'    , 'Incluir &Encabezado de MPU','', 'MPU &Kopfzeilen einfügen');
+chkIncDecVar.Caption := Trans('Include &Variables declaration', 'Incluir Declaración de variables', '',
+                              'Variablendeklaration einfügen');
+RadioGroup2.Caption  := Trans('Style'                  , 'Estilo'                   , '','Stil');
+chkExcUnus.Caption   := Trans('Exclude unused'         , 'Excluir no usadas'        , '','Unbenutzte ausschließen');
+chkIncAddress.Caption:= Trans('Include &Memory Address','Incluir &Dirección de memoria','','Speicheradressen zufügen');
+chkIncComment.Caption:= Trans('Include &Comments'      , 'Incluir &Comentarios'     , '','Kommentare hinzufügen');
+chkIncComment2.Caption:=Trans('Include &Detailed comments', 'Incluir Comentarios &detallados' , '',
+                              '');
 //Output Settings
-tabOutput.Caption    := Trans('Output'               , 'Salida', '','Ausgabe');
-grpOptimLev.Caption  := Trans('Optimization Level:'  , 'Nivel de optimización:', '','Optimierungsstufe');
-grpOptimLev.Items[0] := Trans('Fool'                 , 'Tonto', '','Dumm');
-grpOptimLev.Items[1] := Trans('Smart'                , 'Inteligente', '','Schlau');
+tabOutput.Caption    := Trans('Compiler'               , 'Compilador'               , '', '');
+chkShowErrMsg.Caption:= Trans('&Show Error Messages'   , '&Mostrar mensajes de error', '', '');
+grpOptimLev.Caption  := Trans('Optimization Level:'    , 'Nivel de optimización:'   , '', 'Optimierungsstufe');
+grpOptimLev.Items[0] := Trans('Fool'                   , 'Tonto'                    , '', 'Dumm');
+grpOptimLev.Items[1] := Trans('Smart'                  , 'Inteligente'              , '', 'Schlau');
 
