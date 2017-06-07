d33db825bffd35a7ceb75356f8e9abe703cbb987
Tito Hinostroza <dev_null_1@hotmail.com>
2017-06-07 14:48:50 -0500

    Add files via upload

--- samples/Multiplicación_ASM.pas
+++ samples/Multiplicación_ASM.pas
@@ -1,37 +1,39 @@
+{$PROCESSOR PIC16F84}
+{$FREQUENCY 8Mhz}
 uses PIC16f84A;
 var
   resultado : word;  // Variable global necesaria para devolver el valor de multiplicacion.
-
+ 
 //***********************************************************************
 //  PROCEDIMIENTO: Multiplicacion
 //  Multiplica dos valores de 8 bits.
 //  El resultado queda almacenado en una variable global llamada resultado.
-//  Por ahora PicPas (v.0.6.7) no permite el uso de funciones.
+//  Por ahora PicPas (v.0.6.8) no permite el uso de funciones.
 //***********************************************************************
 procedure Multiplicar (multiplicando, multiplicador : byte);
 begin
 ASM
 ;Inicializacion de Registros
-  BCF STATUS,5              ; RP0=0 / Trabajamos en el Banco de memoria 0.
+  BCF STATUS,STATUS_RP0     ; RP0=0 / Trabajamos en el Banco de memoria 0.
   CLRF resultado.LOW        ; Limpia el byte bajo de la variable global resultado.
   CLRF resultado.HIGH       ; Limpia el byte alto de la variable global resultado.
 ;Comprueba multiplicacion por cero.
   MOVLW $00
   SUBWF multiplicador,W
-  BTFSC STATUS,2
+  BTFSC STATUS,STATUS_Z
   GOTO MULT_FIN             ; Si multiplicador = 0 entonces acabar.
 ;LOOP de multiplicacion
 MULT_LOOP:
   MOVF multiplicando,W      ; Carga el multiplicador en el registro W.
   ADDWF resultado.LOW,F     ; Suma el valor de multiplicando al byte bajo de la variable global resultado
-  BTFSC STATUS,0            ; Comprueba el bit CARRY del registro STATUS.
+  BTFSC STATUS,STATUS_C     ; Comprueba el bit CARRY del registro STATUS.
   INCF resultado.HIGH,F     ; Si CARRY es 0 resultado.LOW se ha desbordado se incrementa resultado.HIGH
   DECFSZ multiplicador,F    ; Decrementa multiplicador y comprueba si ha llegado a cero.
   GOTO MULT_LOOP            ; nuevo paso del bucle de multiplicacion.
 MULT_FIN:
 END
 end;
-
+ 
 begin
   Multiplicar(1,2);
-end. 
+end.
