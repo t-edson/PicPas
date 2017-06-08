f044a2c66303108af6e64272048f6802ca7b1547
Tito Hinostroza <dev_null_1@hotmail.com>
2017-06-07 20:28:09 -0500

    Add files via upload

--- TestCode/Operations/Test2_Word1.pas
+++ TestCode/Operations/Test2_Word1.pas
@@ -106,133 +106,61 @@ begin
 	//////////////////  Operación Mayor /////////////////////
   //////////////////////////////////////////////////////////
 
-//	//Constante
-//  if 0 > 0 then mal else bien end;
-//  if 0 > 1 then mal else bien end;
-//  if 255 > 0 then bien else mal end;
-//  if 255 > 15 then bien else mal end;
-//  if 255 > 255 then mal else bien end;
-//
-//	//Variables
-//  a := 1;  b := 255;
-//  if a > a then mal else bien end;
-//  if a > b then mal else bien end;
-//  if b > b then mal else bien end;
-//  if b > a then bien else mal end;
-//
-//  a := 0;  b := 255;
-//  if a > a then mal else bien end;
-//  if a > b then mal else bien end;
-//  if b > b then mal else bien end;
-//  if b > a then bien else mal end;
-//
-//  a := 0;  b := 1;
-//  if a > a then mal else bien end;
-//  if a > b then mal else bien end;
-//  if b > b then mal else bien end;
-//  if b > a then bien else mal end;
-//
-//  a := 0;  b := 0;
-//  if a > a then mal else bien end;
-//  if a > b then mal else bien end;
-//  if b > b then mal else bien end;
-//  if b > a then mal else bien end;
-//
-//  a := 255;  b := 255;
-//  if a > a then mal else bien end;
-//  if a > b then mal else bien end;
-//  if b > b then mal else bien end;
-//  if b > a then mal else bien end;
-//
-//	//Variables - constantes
-//  a := 1;
-//  if a > 0 then bien else mal end;
-//  if a > 1 then mal else bien end;
-//  if a > 255 then mal else bien end;
-//  if 0 > a then mal else bien end;
-//  if 1 > a then mal else bien end;
-//  if 2 > a then bien else mal end;
-//  if 255 > a then bien else mal end;
-//
-	//Expresiones
-//  a := 10;
-//  if word(0) > a+0 then mal else bien end;
-//  if 0 > a+1 then mal else bien end;
-//  if 11 > a+1 then mal else bien end;
-//  if 12 > a+1 then bien else mal end;
-//  if 255 > a+1 then bien else mal end;
-//
-//  a := 10;
-//  if a+1 > 0 then bien else mal end;
-//  if a+1 > 10 then bien else mal end;
-//  if a+1 > 11 then mal else bien end;
-//  if a+1 > 12 then mal else bien end;
-//  if a+1 > 255 then mal else bien end;
-//
-//  if a > a+1 then mal else bien end;
-//  if a+1 > a then bien else mal end;
-//
-//  if a+1 > a+2 then mal else bien end;
-//  if a+1 > a+1 then mal else bien end;
-//  if a+2 > a+1 then bien else mal end;
-//
-//  //////////////////////////////////////////////////////////
-//	//////////////////  Operación Menor /////////////////////
-//  //////////////////////////////////////////////////////////
-//	{Las pruebas para las operaciones "menor que", no son tan exigentes porque 
-//   se generan a partir de "mayor que"}
-//
-//	//Constante
-//  if 0 < 0 then mal else bien end;
-//  if 0 < 1 then bien else mal end;
-//  if 255 < 0 then mal else bien end;
-//
-//	//Variables
-//  a := 0;  b := 0;
-//  if a < a then mal else bien end;
-//  if a < b then mal else bien end;
-//  if b < a then mal else bien end;
-//
-//  a := 255;  b := 255;
-//  if a < a then mal else bien end;
-//  if a < b then mal else bien end;
-//  if b < a then mal else bien end;
-//
-//	//Variables - constantes
-//  a := 1;
-//  if a < 0 then mal else bien end;
-//  if a < 1 then mal else bien end;
-//  if a < 255 then bien else mal end;
-//  if 0 < a then bien else mal end;
-//  if 255 < a then mal else bien end;
-//
-//	//Expresiones
-//  a := 10;
-//  if a < a+1 then bien else mal end;
-//  if a+1 < a then mal else bien end;
-//
-//  if a+1 < a+2 then bien else mal end;
-//  if a+1 < a+1 then mal else bien end;
-//  if a+2 < a+1 then mal else bien end;
-//
-//  //////////////////////////////////////////////////////////
-//	///////////////  Operación Mayor/Menor o igual ///////////
-//  //////////////////////////////////////////////////////////
-//	{Estas operaciones, son aún menos exigentes, porque son negaciones 
-//  de las operaciones base}
-//
-//	if 5>=4 then bien else mal end;
-//	if 5>=5 then bien else mal end;
-//	if 5>=6 then mal else bien end;
-//
-//  a := 5;  b := 6;
-//  if a >= a then bien else mal end;
-//  if a >= b then mal else bien end;
-//  if b >= a then bien else mal end;
-//
-//	if a <= a then bien else mal end;
-//	if a <= 6 then bien else mal end;
-//	if a <= 4 then mal else bien end;
+	//coConst_Const
+  if word(0) > word(0) then mal else bien end;
+  if word(10) > word(9) then bien else mal end;
+
+	//coConst_Variab
+  a := 0;  
+  if word(0) > a then mal else bien end;
+  a := 1000;  
+  if 1000 > a then mal else bien end;
+  if 1001 > a then bien else mal end;
+  //coConst_Expres
+  a := 0;  
+  if word(1) > (a+1) then mal else bien end;
+  a := 999;  
+  if 1000 > (a+1) then mal else bien end;
+  if 1001 > (a+1) then bien else mal end;
+  //coVariab_Const
+  //<<No implemented>>
+
+  //coVariab_Variab
+  a := 0; b := 0; 
+  if a > b then mal else bien end;
+  a := 256; b := 255; 
+  if a > b then bien else mal end;
+  if b > a then mal else bien end;
+  a := 1000; b := 1000; 
+  if a > b then mal else bien end;
+  a := 1001; b := 1000; 
+  if a > b then bien else mal end;
+  //coVariab_Expres
+  a := 1; b := 0; 
+  if a > (b+1) then mal else bien end;
+  a := 256; b := 254; 
+  if a > (b+1) then bien else mal end;
+  if a > (b+2) then mal else bien end;
+  a := 1001; b := 1000; 
+  if a > (b+1) then mal else bien end;
+  a := 1001; b := 999; 
+  if a > (b+1) then bien else mal end;
+  //coExpres_Const
+  //<<No implemented>>
+
+  //coExpres_Variab
+  //<<No implemented>>
+
+  //coExpres_Expres
+  a := 0; b := 0; 
+  if (a+1) > (b+1) then mal else bien end;
+  a := 254; b := 254; 
+  if (a+1) > (b+1) then mal else bien end;
+  if (a+2) > (b+1) then bien else mal end;
+  a := 1000; b := 1000; 
+  if (a+1) > (b+2) then mal else bien end;
+  if (a+1) > (b+1) then mal else bien end;
+  if (a+2) > (b+1) then bien else mal end;
 
 end.
 
