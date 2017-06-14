88229e897af1c2c1b34d9b479a4f6e6c309c58d1
Tito Hinostroza <dev_null_1@hotmail.com>
2017-06-14 16:05:22 -0500

    Add files via upload

--- samples/Multiplicacion_ASM.pas
+++ samples/Multiplicacion_ASM.pas
@@ -28,35 +28,35 @@ var
                     // en los puertos A y E.
  
 //***********************************************************************
-//  PROCEDIMIENTO: Multiplicacion
+//  FUNCION: Multiplicar
 //  Multiplica dos valores de 8 bits.
 //  Devuelve el resultado en variable tipo word de 16 bits.
 //***********************************************************************
 procedure Multiplicar (multiplicando, multiplicador : byte) : word;
 var
-  resultado : word; // Variable global para mostrar el resultado en display.
+  multiplicacion : word;
 begin
-  ASM
-  ;Inicializacion de Registros
-    BCF STATUS,bit_RP0        ; RP0=0 / Trabajamos en el Banco de memoria 0.
-    CLRF resultado.LOW        ; Limpia el byte bajo de la variable global resultado.
-    CLRF resultado.HIGH       ; Limpia el byte alto de la variable global resultado.
-  ;Comprueba multiplicacion por cero.
-    MOVLW $00
-    SUBWF multiplicador,W
-    BTFSC STATUS,bit_Z
-    GOTO MULT_FIN             ; Si multiplicador = 0 entonces acabar.
-  ;LOOP de multiplicacion
-  MULT_LOOP:
-    MOVF multiplicando,W      ; Carga el multiplicador en el registro W.
-    ADDWF resultado.LOW,F     ; Suma el valor de multiplicando al byte bajo de la variable global resultado
-    BTFSC STATUS,bit_C        ; Comprueba el bit CARRY del registro STATUS.
-    INCF resultado.HIGH,F     ; Si CARRY es 0 resultado.LOW se ha desbordado se incrementa resultado.HIGH
-    DECFSZ multiplicador,F    ; Decrementa multiplicador y comprueba si ha llegado a cero.
-    GOTO MULT_LOOP            ; nuevo paso del bucle de multiplicacion.
-  MULT_FIN:
-  END
-  exit(resultado);
+ASM
+;Inicializacion de Registros
+  BCF STATUS_RP0                 ; RP0=0 / Trabajamos en el Banco de memoria 0.
+  CLRF multiplicacion.LOW        ; Limpia el byte bajo de la variable global resultado.
+  CLRF multiplicacion.HIGH       ; Limpia el byte alto de la variable global resultado.
+;Comprueba multiplicacion por cero.
+  MOVLW $00
+  SUBWF multiplicador,W
+  BTFSC STATUS_Z
+  GOTO MULT_FIN                  ; Si multiplicador = 0 entonces acabar.
+;LOOP de multiplicacion
+MULT_LOOP:
+  MOVF multiplicando,W           ; Carga el multiplicador en el registro W.
+  ADDWF multiplicacion.LOW,F     ; Suma el valor de multiplicando al byte bajo de la variable global resultado
+  BTFSC STATUS_C                 ; Comprueba el bit CARRY del registro STATUS.
+  INCF multiplicacion.HIGH,F     ; Si CARRY es 0 resultado.LOW se ha desbordado se incrementa resultado.HIGH
+  DECFSZ multiplicador,F         ; Decrementa multiplicador y comprueba si ha llegado a cero.
+  GOTO MULT_LOOP                 ; nuevo paso del bucle de multiplicacion.
+MULT_FIN:
+END
+exit(multiplicacion);
 end;
  
 //***********************************************************************
