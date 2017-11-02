2d87b595258262e7c6e3c644b2705539dbcbc68c
Tito Hinostroza <dev_null_1@hotmail.com>
2017-11-02 10:25:00 -0500

    Add files via upload

---
+++ samples/ASM_BlinkLed.pas
@@ -0,0 +1,27 @@
+{Sample program to blink a Led on PORTB.0
+using ASM and a Pascal variable.}
+{$FREQUENCY 4 MHZ }
+{$PROCESSOR PIC16F84A}
+uses PIC16F84A;
+var counter: byte;
+begin
+asm
+;------------- SETUP ---------------
+        BSF   STATUS, STATUS_RP0
+        CLRF  PORTB
+        BCF   STATUS, STATUS_RP0
+;----------  MAIN LOOP -------------
+INICIO: 
+        BSF   PORTB, 0 ;led ON
+        CALL  ESPERA   ;delay
+        BCF   PORTB, 0 ;led OFF
+        CALL  ESPERA   ;delay
+        GOTO  INICIO
+;-------- SUBROUTINES --------------
+ESPERA:
+        CLRF COUNTER
+LOOP1:  DECFSZ COUNTER, F
+        GOTO LOOP1
+        RETURN
+end
+end.
