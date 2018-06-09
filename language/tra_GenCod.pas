5f01273553289d4412ba21d9554ab52d15dd1d75
Tito Hinostroza <dev_null_1@hotmail.com>
2018-06-08 21:21:35 -0500

    Add files via upload

---
+++ language/tra_GenCod.pas
@@ -0,0 +1,12 @@
+MSG_NOT_IMPLEM := Trans('Not implemented.', 'No implementado' , '',
+                        ''          , '', '',
+                        '');
+MSG_INVAL_PARTYP:= Trans('Invalid parameter type: %s','Tipo de parámetro inválido: %s', '',
+                        ''          , '', '',
+                        '');
+MSG_UNSUPPORTED := Trans('Unsupported','No soportado', '',
+                        ''          , '', '',
+                        '');
+MSG_CANNOT_COMPL:= Trans('Cannot Compile: "%s"','No se puede compilar: %s', '',
+                        ''          , '', '',
+                        '');
---
+++ language/tra_GenCodBas.pas
@@ -0,0 +1,22 @@
+TXT_SAVE_W      := Trans(';save W'   , ';guardar W'   , '',
+                         ''          , '', '',
+                         '');
+TXT_SAVE_Z      := Trans(';save Z'   , ';guardar Z'   , '',
+                         ''          , '', '',
+                         '');
+TXT_SAVE_H      := Trans(';save H'   , ';guardar H'   , '',
+                         ''          , '', '',
+                         '');
+MSG_NO_ENOU_RAM:= Trans('No enough RAM', 'No hay suficiente RAM.'  , '',
+                        ''          , '', '',
+                        '');
+MSG_VER_CMP_EXP:= Trans('Very complex expression. To simplify.','Expresión muy compleja. Simplificar.' , '',
+                        ''          , '', '',
+                        '');
+MSG_STACK_OVERF:= Trans('Stack Overflow', 'Desborde de pila.' , '',
+                        ''          , '', '',
+                        '');
+MSG_NOT_IMPLEM := Trans('Not implemented.', 'No implementado' , '',
+                        ''          , '', '',
+                        '');
+
